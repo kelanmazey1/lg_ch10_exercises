16722a6cce3a7cb771f5d6352dbf43442271cb26
Kelan Mazey <kelanmazey@gmail.com>
2025-10-22 21:45:13 +0700

    added comments

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
+// Package for the exercises from chapter 10 of Learing Go.
 package lgch10exercises
 
+//
+// [Adds]: https://www.mathsisfun.com/numbers/addition.html 2 integers together and returns result
 func Add(a, b int) int {
 	return a + b
 }
