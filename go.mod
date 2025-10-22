8d92ae07a5a2192ae4ab042040b2b262305d5266
Kelan Mazey <kelanmazey@gmail.com>
2025-10-22 21:55:53 +0700

    Adds number interface and updates Add to be method

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kelanmazey1/lg_ch10_exercises
 
 go 1.25.1
+
+require golang.org/x/exp v0.0.0-20251017212417-90e834f514db
--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 // Package for the exercises from chapter 10 of Learing Go.
 package lgch10exercises
 
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 //
 // [Adds]: https://www.mathsisfun.com/numbers/addition.html 2 integers together and returns result
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
