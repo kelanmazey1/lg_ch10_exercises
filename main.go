e5afcc3db2f1fbf3b335a009599fe199f9d956f1
Kelan Mazey <kelanmazey@gmail.com>
2025-10-23 09:16:43 +0700

    updating comments

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package lgch10exercises
 
 import "golang.org/x/exp/constraints"
 
+// Type to group Integer and Float [https://golang.org/x/exp/constraints]
 type Number interface {
 	constraints.Integer | constraints.Float
 }
 
+// Adds  2 integers together and returns result
 //
-// [Adds]: https://www.mathsisfun.com/numbers/addition.html 2 integers together and returns result
+// See: https://www.mathsisfun.com/numbers/addition.html
 func Add[T Number](a, b T) T {
 	return a + b
 }
